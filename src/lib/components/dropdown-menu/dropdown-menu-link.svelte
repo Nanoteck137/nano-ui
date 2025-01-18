fe4fc8dd2adac1fd5619ef51c5347ede9dbe3494
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-01-18 08:48:24 +0100

    Added cursor-pointer to dropdown items/links

--- src/lib/components/dropdown-menu/dropdown-menu-link.svelte
+++ src/lib/components/dropdown-menu/dropdown-menu-link.svelte
@@ -17,7 +17,7 @@
 <DropdownMenuPrimitive.Item
   bind:ref
   class={cn(
-    "relative flex h-full w-full cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50 [&>svg]:size-4 [&>svg]:shrink-0",
+    "relative flex h-full w-full cursor-pointer select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50 [&>svg]:size-4 [&>svg]:shrink-0",
     inset && "pl-8",
     className,
   )}
